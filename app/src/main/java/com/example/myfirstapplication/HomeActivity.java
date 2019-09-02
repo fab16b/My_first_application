99dd6cdd3e54b7f9e02182c55964e1dace08ffa9
Felix Mbikogbia <mbikogbiafelix@gmail.com>
2019-09-02 08:14:51 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapplication/HomeActivity.java
@@ -0,0 +1,36 @@
+package com.example.myfirstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class HomeActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    //** called when the user taps the send button */
+    public void sendMessage(View view) {
+        //Do something in response to button
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
+}
+
+
